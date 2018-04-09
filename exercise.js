ec3ba8804eaac3d86c124005fd9fc50e6f947cce
abrocks <abrocks@mit.edu>
2018-04-09 10:18:06 -0400

    class demo

--- exercise.js
+++ exercise.js
@@ -3,12 +3,14 @@ var init = function(){
     // setup canvas
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
+    //a canvas is a place where you can draw to the screen 
 
     // generate 10 random points
     var length = 10;
     var points = [];
     for(var i=0; i<length; i++){
         var x = random();
+        //he wrote a function random 
         var y = random();   
         circle(context,x,y,'blue');
         points.push({x:x,y:y});
@@ -23,13 +25,25 @@ var init = function(){
 
 
 var calculateCentroid = function(points){
-    //----------------------------------
-    //  YOUR CODE
-    //----------------------------------    
-
+    //wil recieve an array of points 
+    var sumX = 0; 
+    var sumY = 0; 
+    points.forEach(function(element){
+        sumX += element.x;
+        sumY += element.y; 
+    }); 
+    var CX = sumX/points.length;
+    var CY = sumY/points.length;
+    return {x: Math.floor(CX), y:Math.floor(CY)};
+    //why can we just type x: here  
     // return centroid with the following format
     // return {x:10, y:30};  
 };
+/*
+unsupervised learning means no training 
+
+*/
+
 
 
 function circle(context,x,y,fillStyle) {
